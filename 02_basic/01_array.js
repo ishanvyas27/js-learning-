94b13ea3924c78f3b87e4095c2167a754df142ce
ishanvyas27 <162148973+ishanvyas27@users.noreply.github.com>
2025-01-04 12:56:40 +0000

    Array and its method ( two are more array joining)  //

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -13,8 +13,25 @@ const DcHero = ["SuperMan" , "BatMan", "Flash"];
 
 // using concat function
 
-const allHeros = marvelHero.concat(DcHero)
-console.log(allHeros);
+// const allHeros = marvelHero.concat(DcHero)
+// console.log(allHeros);
 
-const all_new_heros = [...marvelHero ,...DcHero]
-console.log(all_new_heros);
+// const all_new_heros = [...marvelHero ,...DcHero]
+// console.log(all_new_heros);
+
+console.log(Array.isArray("Ishan"));//check for the array
+
+console.log(Array.from("Ishan"));
+// creates the array from the non array elemnet 
+console.log(Array.from({ name : "Ishan"})); //Intrestring
+
+
+const s1 = 100
+const s2 = 200
+const s3 = 300
+
+// creates the arrray by using elements uinsg (.of)
+console.log(Array.of(s1 , s2, s3));
+
+
+
