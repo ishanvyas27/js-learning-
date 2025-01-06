ff2c32ab9c4b3a1b461d815f28c5192cd6395106
ishanvyas27 <162148973+ishanvyas27@users.noreply.github.com>
2025-01-06 10:18:09 +0000

    loops

---
+++ 05_iterator/for.js
@@ -0,0 +1,13 @@
+// for loop
+
+for(let j = 0 ; j < 20 ; j++){
+   // console.log(index);
+    
+}
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop ${i}`);
+    for (let j = 0; j <= 10; j++) {
+      console.log(`Inner loop ${j}`);
+      
+    }
+}
