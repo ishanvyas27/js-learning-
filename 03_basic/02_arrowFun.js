fa4124a7061f56358cf5e6de2f0ad151b1539082
ishanvyas27 <162148973+ishanvyas27@users.noreply.github.com>
2025-01-06 08:16:43 +0000

    Arrow Function

---
+++ 03_basic/02_arrowFun.js
@@ -0,0 +1,65 @@
+// this function
+
+// const user ={
+//     userName : "Ishan",
+//     age : 18,
+//     greeting : function (){
+//         console.log(`Hello! ${this.userName} how your doing?`);
+        
+//     }
+// }
+
+// //console.log(user.greeting());
+
+// // console.log(user.userName = "krish");
+// // console.log(user.greeting());
+// // console.log(this);
+
+
+// // this keyword only works with object not with function
+// // chai() can acces the function defore decleration
+// function chai(){
+//    let name ="Ishan"
+//     console.log(this.name);
+    
+// }
+
+// //chai1() cannot access the function before decleration  %% In Case of function assign to variable 
+// const chai1 = function(){
+//     let name = "IShan"
+//     console.log(this.name);
+    
+// }
+// chai1()
+
+// Arrow Function  () =>{}
+
+//     const chai1 = () => {
+//            let name = "Ishan"
+//           console.log(this);
+//         }
+// chai1()
+
+
+// Arrow function implementaion 
+// const addTwoNum = (num1 , num2) =>{
+//     return num1+num2
+// }
+// console.log(addTwoNum(4 ,5));
+
+// implicit return 
+
+//const addTwoNum = (num1 , num2) => num1+num2
+
+
+// doesnt need to right return keyword just right the statemnt in ( )
+// const addTwoNum = (num1 , num2) => (num1 + num2)
+// console.log(addTwoNum(4 ,5));
+
+// object can be return just by writing the object statement inside the ( )
+
+// const chai = (num1 , num2) => ({ username : "Ishan "})
+
+// console.log(chai(10 , 12));
+  
+console.log("Hello array");
