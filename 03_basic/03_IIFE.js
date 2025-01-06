e85228d280a6dfd9fecda530bb54ebb5f84e159c
ishanvyas27 <162148973+ishanvyas27@users.noreply.github.com>
2025-01-06 09:20:12 +0000

    Imediately Invoked function Expression IIFE

---
+++ 03_basic/03_IIFE.js
@@ -0,0 +1,22 @@
+// Imediately Invoked function Expression IIFE
+
+
+// ( function definition )( function call);
+
+// ( function cahi() {
+//     console.log(`DB Connected`);
+    
+// })();
+
+// ( () => {
+//     console.log(`DB Connected`);
+    
+// })()
+
+
+// Pasing argumnets in IIFT function
+((name) =>{
+    console.log(`Hello ${name}`);
+    
+})("Ishan")
+
